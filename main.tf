03870234f7ee692cb0b89878fda94914d685afef
tgaffney <tgaffney@missioncloud.com>
2020-09-28 13:17:59 -0400

    updating modules

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-test
+odule "vpc" {
+  source = "../../modules/vpc"
+
+  name = "development-vpc"
+  cidr = "10.180.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.180.1.0/24", "10.180.2.0/24"]
+  public_subnets  = ["10.180.101.0/24", "10.180.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false  
+
+  tags = {
+    Terraform = "true"
+    Environment = "development"
+  }
+}
---
+++ modules/vpc/vpc_flow_logs.tf
@@ -0,0 +1,54 @@
+resource "aws_flow_log" "vpc_flow_logs" {
+  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.this[count.index].id
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name_prefix = var.name
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name_prefix = var.name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs" {
+  name_prefix = var.name
+  role = aws_iam_role.vpc_flow_logs.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
